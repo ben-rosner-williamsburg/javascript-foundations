b7bb34b7d3926af347f09e5b0def520493488ea1
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-08-29 09:47:34 -0400

    Complete dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,40 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon;
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+function eat(dragon) {
+  dragon.timesEaten++;
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+
+function findFireBreathers(dragons) {
+  var fireBreathers = [];
+  for (var i = 0; i < dragons.length; i++) {
+    if (dragons[i].temperment === "aggressive") {
+      fireBreathers.push(dragons[i]);
+    }
+  }
+  return fireBreathers;
+}
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers
 }
