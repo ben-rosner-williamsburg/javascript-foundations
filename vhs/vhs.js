91db04de2730e94be44ec3d63a260d309c8aec2c
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-09-11 15:17:03 -0400

    Complete vhs tests

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,37 @@
+function createTape(title,readyToPlay = false) {
+    var tape = {
+        title: title,
+        readyToPlay: readyToPlay
+    }
+    return tape;
+}
 
+function reset(tape){
+    if (tape.readyToPlay === false){
+      tape.readyToPlay = true;
+    }
+    return tape;
+}
 
-module.exports = {  }
+function createCollection(tapeOne, tapeTwo, tapeThree){
+    if (!tapeOne || !tapeTwo || !tapeThree){
+        return `Your collection is empty.`
+    }
+    var collection = [];
+    collection.push(tapeOne, tapeTwo, tapeThree);
+    return collection;
+}
+
+function previewCollection(collection){
+    var titles = []
+    for (var i = 0; i < collection.length; i++) {
+        titles.push(collection[i].title)
+    }
+    return titles;
+}
+module.exports = {
+    createTape,
+    reset,
+    createCollection,
+    previewCollection
+}
